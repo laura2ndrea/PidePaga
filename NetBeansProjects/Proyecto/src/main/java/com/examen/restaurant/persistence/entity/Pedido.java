b723723de9b69988785262949b7a4c6bebf4a2eb
camper <camper@camper-9048-900-0017>
2025-02-11 13:05:54 +0100

    feat: configuration

---
+++ NetBeansProjects/Proyecto/src/main/java/com/examen/restaurant/persistence/entity/Pedido.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.examen.restaurant.persistence.entity;
+
+/**
+ *
+ * @author camper
+ */
+public class Pedido {
+    
+}
