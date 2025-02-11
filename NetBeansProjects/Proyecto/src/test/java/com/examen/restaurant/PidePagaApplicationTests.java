80b3a48121346df8c4056b5428fda246ebc3f6e7
camper <camper@camper-9048-900-0017>
2025-02-11 12:36:58 +0100

    feat: configuration

---
+++ NetBeansProjects/Proyecto/src/test/java/com/examen/restaurant/PidePagaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examen.restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PidePagaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
